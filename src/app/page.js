04cd434d44f96004a1837e14acaf67499f024164
rumon5h <mdrumonkhan48519@gmail.com>
2023-09-16 14:02:51 +0600

    updated done

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Banner from "@/components/banner/Banner";
+import QuranBanner from "@/components/quranBanner/QuranBanner";
 
 export default function Home() {
   return (
    <main>
     <Banner />
+    <QuranBanner/>
 
    </main>
   )
